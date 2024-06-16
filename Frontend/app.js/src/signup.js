426c116ac94fa3b042f8c3a209e03c1be4b18760
Muskan Asthana <m24.asthana@gmail.com>
2024-06-17 01:34:01 +0530

    Edited server.js

--- Frontend/app.js/src/signup.js
+++ Frontend/app.js/src/signup.js
@@ -1,22 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 
 function Signup() {
+    const [values, setValues] = useState({
+        name: '',
+        countrycode: '',
+        phone: ''
+    })
+    const handleChange = (event) => {
+        setValues({...values, [event.target.name]:[event.target.value]})
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:8081', values)
+        .then(res => console.log("Registered Successfully!!"))
+        .catch(err => console.log(err));
+    }
     return (
         <div className='d-flex w-100 vh-100 bg-primary justify-content-center align-items-center'>
             <div className='bg-white p-3 rounded w-25'>
                 <h2>Sign-Up</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className='mb-3'>
                         <label htmlFor='name'><strong>Name</strong></label>
-                        <input type='text' placeholder='Enter Name' name='name' className='form-control rounded-0'/>
+                        <input type='text' placeholder='Enter Name' name='name' className='form-control rounded-0' onChange={handleChange}/>
                     </div>                    
                     <div className='mb-3'>
-                        <label htmlFor='email'><strong>Email</strong></label>
-                        <input type='email' placeholder='Enter Email' name='email' className='form-control rounded-0'/>
+                        <label htmlFor='country-code'><strong>Country Code</strong></label>
+                        <input type='text' placeholder='Enter Country Code' name='country-code' className='form-control rounded-0' onChange={handleChange}/>
                     </div>
                     <div className='mb-3'>
-                        <label htmlFor='password'><strong>Password</strong></label>
-                        <input type='password' placeholder='Enter Password' name='password' className='form-control rounded-0'/>
+                        <label htmlFor='phone'><strong>Phone</strong></label>
+                        <input type='text' placeholder='Enter phone no.' name='phone' className='form-control rounded-0' onChange={handleChange}/>
                     </div>
                     <button type='submit' className='btn btn-success w-100 rounded-0'>Sign up</button>
                     <p>You agree to our terms and policies</p>
