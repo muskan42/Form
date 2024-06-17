103db1d44366c2e4dc911e499005bebeb879b3e6
Muskan Asthana <m24.asthana@gmail.com>
2024-06-17 10:26:37 +0530

    modified server.js and signup.js

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,28 @@ const cors = require('cors')
 
 const app = express()
 app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: "root",
+    phone: '',
+    database: 'signup'
+})
+
+app.post('/signup', (req, res) => {
+    const sql = "INSERT INTO login (`name`, `countrycode`, `phone`) Values(?)";
+    const values = [
+        req.body.name,
+        req.body.countrycode,
+        re.body.phone,
+    ]
+})
+
+db.query(sql, [values], (err, data) => {
+    if(err) return res.json(err);
+    return res.json(data);
+})
 
 app.listen(8081, ()=> {
     console.log("Listening...")
